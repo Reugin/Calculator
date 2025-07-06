f7ba1c8b4976ae5117a82cbe78dcd89a784d58d8
Reugin <abhigaikwad735@gmail.com>
2025-07-07 00:17:12 +0530

    Calculator UI and Script

---
+++ script.js
@@ -0,0 +1,32 @@
+let input = document.getElementById('inputarea');
+let buttons = document.querySelectorAll('button');
+let resultdisplay = document.getElementById('resultdisplay');
+let del = document.getElementById('del');
+
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let value = e.target.innerHTML;
+        if (value == '=') {
+            try {
+                string = eval(string);
+                resultdisplay.innerHTML = "= "+string;
+                console.log("result" + string);
+            } catch (err) {
+                resultdisplay.innerHTML = "Error"
+            }
+        } 
+        else if (value == 'Ac') {
+            string = "";
+            input.value = string
+            resultdisplay.innerHTML = string
+        }
+        else {
+            string += value;
+            input.value = string;
+        }
+    })
+
+})
